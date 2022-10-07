0f7e08a7447515dd114566d36ef4fbef9be2670d
0Smile <cornelg7@yahoo.com>
2022-10-07 07:44:52 +0000

    Fixed small screen resolutions, adjusted colors, adjusted favicon

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-testa
+# Live at [piqbubble](https://piqbubble.com)!
+
 # Welcome to [Astro](https://astro.build)
 
 [![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz.svg)](https://stackblitz.com/github/withastro/astro/tree/latest/examples/basics)
--- src/utils/General.utils.ts
+++ src/utils/General.utils.ts
@@ -2,15 +2,21 @@ export const colors = {
     'light-1': '#F5F6F4',
     'light-2': '#E0E2DB',
     'light-3': '#D2D4C8',
-    'dark-1':  '#30321C',
+    'dark-1':  '#756d75',
     'accent-1': '#C585B3',
     'accent-2': '#BC69AA',
-    'color-logo-1': '#756d75',
-    'color-logo-2': '#c586b3',
+    'logo-1': '#756d75',
+    'logo-2': '#c586b3',
 }
 
 export const getColors = (light: boolean) => {
     return light 
         ? {backgroundColor: colors['light-2'], foregroundColor: colors['dark-1']} 
         : {backgroundColor: colors['dark-1'], foregroundColor: colors['light-2']};
+}
+
+export const getLogoColors = (light: boolean) => {
+    return light 
+        ? {dark: colors['dark-1'], accent: colors['logo-2']} 
+        : {dark: colors['light-2'], accent: colors['logo-2']};
 }
